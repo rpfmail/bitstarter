8db6e5534776a4838aa929e710ded56135fc3b66
Ryan <rpfmail@gmail.com>
2013-07-10 19:15:20 +0000

    Attempting to change to reading index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file_buffer=fs.readFileSync('index.html');
+  var text=file_buffer.toString('utf8');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
